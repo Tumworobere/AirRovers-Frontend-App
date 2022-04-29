d83483335f500fa4a6924781058e67b2a9eef89b
alexander16108 <alexandermayowa05@gmail.com>
2022-04-29 15:25:54 +0100

    Adding pages for navbar

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+const ErrorPage = () => (
+  <div>
+    <h1>Page not Found</h1>
+    <p>
+      Either you messed up or you&apos;re trying to break the app...
+      However, nice try.
+    </p>
+  </div>
+);
+
+export default ErrorPage;
---
+++ src/pages/Reservations.js
@@ -0,0 +1,8 @@
+const Reservations = () => (
+  <div>
+    <h1>Reservations</h1>
+    <p>You will be able to see your reservations in this page.</p>
+  </div>
+);
+
+export default Reservations;
