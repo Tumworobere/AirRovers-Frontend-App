885ed901d7d948de5d42b256f7881782a8946cb2
alexander16108 <alexandermayowa05@gmail.com>
2022-05-01 23:13:35 +0100

    fixing display errors

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Airplanes from './pages/Airplanes';
+import Reservations from './pages/Reservations';
+import Reserve from './pages/Reserve';
+import AddAirplane from './pages/addAirplane';
+import RemoveAirplane from './pages/RemoveAirplane';
+import ErrorPage from './pages/ErrorPage';
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <h2>bootstrap</h2>
-      <button type="button" className="btn btn-danger">Danger</button>
-    </div>
-  );
-}
+const App = () => (
+  <>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Airplanes />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/add-reservation" element={<Reserve />} />
+        <Route path="/add-airplane" element={<AddAirplane />} />
+        <Route path="/remove-airplane" element={<RemoveAirplane />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </Router>
+  </>
+
+);
 
 export default App;
---
+++ src/pages/Airplanes.js
@@ -0,0 +1,8 @@
+const Airplanes = () => (
+  <div>
+    <h1>Airplanes</h1>
+    <p>You will be able to see the Airplanes in this page.</p>
+  </div>
+);
+
+export default Airplanes;
