f8b2fad7752f9e9ef6b738b6c97e57a102444b67
Kingstalux <kingsleyngu25@gmail.com>
2022-05-04 04:43:06 +0100

    Add airplane page

--- src/pages/Airplanes.js
+++ src/pages/Airplanes.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+// import { useNavigate, NavLink } from 'react-router-dom';
+import { fetchData } from '../redux/airplanes/airplanes';
 
 export default function Airplanes() {
+  const dispatch = useDispatch();
+  // const navigate = useNavigate();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, []);
+
+  const dataArray = useSelector((state) => state.planeReducer.data);
+
+  const airplanes = dataArray.map((plane) => (
+    <div className="plane-card" key={plane.id}>
+      <img src={plane.images} alt="airplane" />
+    </div>
+  ));
+
   return (
-    <div>Airplanes</div>
+    <div className="airplanes">
+      {airplanes}
+    </div>
   );
 }
