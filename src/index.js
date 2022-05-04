36db98d8695162eadda1a0ae353dadf69ed37817
Kingstalux <kingsleyngu25@gmail.com>
2022-05-04 04:24:02 +0100

    Add provider

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
+import store from './redux/configureStore';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
