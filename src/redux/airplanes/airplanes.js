8cfe7da721b1e6ba8a7db8c85c827263c1070f14
Kingstalux <kingsleyngu25@gmail.com>
2022-05-04 04:20:28 +0100

    Add reducer

--- src/redux/airplanes/airplanes.js
+++ src/redux/airplanes/airplanes.js
@@ -23,3 +23,68 @@ const fetchDataFailure = (error) => ({
 });
 
 const url = 'https://aviones-api.herokuapp.com/planes';
+
+function handleErrors(response) {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+}
+
+export function fetchData() {
+  return (dispatch) => {
+    dispatch(fetchDataBegin());
+    return fetch(url)
+      .then(handleErrors)
+      .then((res) => res.json())
+      .then((json) => {
+        const data = [];
+        json.forEach((element) => {
+          const plane = {
+            id: element.id,
+            name: element.name,
+            capacity: element.capacity,
+            range: element.range,
+            url: element.url,
+            speed: element.speed,
+            images: element.images,
+            price: element.price,
+          };
+          data.push(plane);
+        });
+        dispatch(fetchDataSuccess(data));
+      })
+      .catch((error) => dispatch(fetchDataFailure(error)));
+  };
+}
+
+const planeReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case FETCH_DATA_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+
+    case FETCH_DATA_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        data: action.payload,
+      };
+
+    case FETCH_DATA_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        data: [],
+      };
+
+    default:
+      return state;
+  }
+};
+
+export default planeReducer;
