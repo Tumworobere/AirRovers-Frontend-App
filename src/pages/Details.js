5f2f509d0fb1ae21f9973787e0c0b6c15d59bdc4
Kingstalux <kingsleyngu25@gmail.com>
2022-05-05 15:26:33 +0100

    Style details page

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,22 +1,54 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { NavLink } from 'react-router-dom';
+import '../App.css';
 
 export default function Details() {
   const planeStats = useSelector((state) => state.detailsReducer.data);
 
   const plane = planeStats.map((plane) => (
-    <div key={plane.id}>
-      <img
-        className="d-block w-100 img"
-        src={plane.images}
-        alt="airplane"
-      />
+    <div key={plane.id} className="contain">
+      <div className="image">
+        <img
+          className="d-block w-100 img"
+          src={plane.images}
+          alt="airplane"
+        />
+      </div>
+      <div className="info">
+        <h2>{plane.name}</h2>
+        <div>
+          <p>capacity</p>
+          <p>{plane.capacity}</p>
+        </div>
+
+        <div>
+          <p>Speed</p>
+          <p>{plane.speed}</p>
+        </div>
+
+        <div>
+          <p>Range</p>
+          <p>{plane.range}</p>
+        </div>
+
+        <div>
+          <p>Price</p>
+          <p>
+            $
+            {plane.price}
+          </p>
+        </div>
+        <NavLink to="/add-reservation">
+          Reserve
+        </NavLink>
+      </div>
     </div>
   ));
 
   return (
-    <div>
+    <>
       { plane }
-    </div>
+    </>
   );
 }
