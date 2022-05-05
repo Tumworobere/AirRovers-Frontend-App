6681fbe2e99647f606b03ed0683c7ceb4f42810f
Annah Tomoro <annaht.annah@gmail.com>
2022-05-05 13:33:17 +0500

    Update Reserve.js with the reserve form

--- src/pages/Reserve.js
+++ src/pages/Reserve.js
@@ -6,6 +6,38 @@ const Reserve = () => (
       Airplane using that form.
     </p>
     <p>Note: only admins should be able to add Airplanes.</p>
+    <form>
+            <div className="reserve-form">
+              <div className="city-style">
+                <select
+                  id="cityId"
+                  placeholder="city"
+                  value={selectedCity}
+                  onChange={(e) => setSelectedCity(e.target.value)}
+                >
+                  <option>---Select City---</option>
+                  {cities.map((city) => <option key={city.id}>{city.name}</option>)}
+                </select>
+              </div>
+              <div className="date-style">
+                <DropdownDate
+                  startDate={
+            TodayDate()
+          }
+                  endDate={
+            extendDate(5)
+          }
+                  selectedDate={
+            selectDate
+          }
+                  onDateChange={(date) => {
+                    setSelectedDate(formatDate(date));
+                  }}
+                />
+              </div>
+              <button type="button" onClick={handleSubmit}>Reserve</button>
+            </div>
+    </form>
   </div>
 );
 
