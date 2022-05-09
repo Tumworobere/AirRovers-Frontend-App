3227e6e3f0d896dc39aaedfbcff43b7ab6ee569a
Kingstalux <kingsleyngu25@gmail.com>
2022-05-09 20:27:17 +0100

    Style slider

--- src/pages/Airplanes.js
+++ src/pages/Airplanes.js
@@ -1,10 +1,37 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable react/jsx-props-no-spreading */
+
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import Carousel from 'react-bootstrap/Carousel';
 import { NavLink } from 'react-router-dom';
+import Slider from 'react-slick';
+import { FaChevronCircleRight, FaChevronCircleLeft } from 'react-icons/fa';
 import { fetchAirplanes } from '../redux/airplanes/airplanes';
 import { fetchPlaneStats } from '../redux/details/details';
-import '../App.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import './slider.css';
+
+const PrevBtn = (props) => {
+  const { className, onClick } = props;
+
+  return (
+    <div className={className} onClick={onClick}>
+      <FaChevronCircleLeft style={{ color: 'red' }} />
+    </div>
+  );
+};
+const NextBtn = (props) => {
+  const { className, onClick } = props;
+
+  return (
+    <div className={className} onClick={onClick}>
+      <FaChevronCircleRight style={{ color: 'red' }} />
+    </div>
+  );
+};
 
 export default function Airplanes() {
   const dispatch = useDispatch();
@@ -20,39 +47,67 @@ export default function Airplanes() {
   const dataArray = useSelector((state) => state.airplanes.airplanes_arr);
 
   const airplanes = dataArray.map((plane) => (
-    <Carousel.Item key={plane.id} className="item h-screen w-screen justify-center">
+
+    <div className="card" key={plane.id}>
       <img
-        className="d-block w-100 img justify-center"
         src={plane.images}
         alt="airplane"
       />
-      <Carousel.Caption>
-        <NavLink to="/details" className="mt-0 p-0 text-center font-bold text-green-700 uppercase" onClick={pageChange} id={plane.id}>
-          <h3 id={plane.id}>{plane.name}</h3>
-        </NavLink>
-      </Carousel.Caption>
-    </Carousel.Item>
+      <NavLink to="/details" className="mt-0 p-0 text-center font-bold text-green-700 uppercase" onClick={pageChange} id={plane.id}>
+        <h3 id={plane.id}>{plane.name}</h3>
+      </NavLink>
+      <p>{plane.speed}</p>
+      <p>{plane.range}</p>
+    </div>
+
   ));
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          initialSlide: 2,
+        },
+      },
+    ],
+  };
+
   return (
-    <>
-      <section className="homepage">
-        <div>
-          <h1>
-            Airplane Models
-          </h1>
 
-          <p>
-            Please select an airplane model
-          </p>
-        </div>
+    <div className="contain">
 
-        <div className="airplanes">
-          <Carousel fade variant="dark" className="container">
-            {airplanes}
-          </Carousel>
-        </div>
-      </section>
-    </>
+      <div className="heading">
+        <h1>Air Rover Models</h1>
+
+        <p>Please select a model</p>
+      </div>
+
+      <div className="container">
+
+        <Slider
+          {...settings}
+          prevArrow={<PrevBtn />}
+          nextArrow={<NextBtn />}
+        >
+          {airplanes}
+        </Slider>
+
+      </div>
+    </div>
   );
 }
