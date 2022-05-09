213b9211ec497164974f31c3b40f4e8cffddc89c
alexander16108 <alexandermayowa05@gmail.com>
2022-05-10 00:23:07 +0100

    updating stylings in desktop version

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -9,16 +9,16 @@ export default function Details() {
   const planeStats = useSelector((state) => state.details.data);
 
   const plane = planeStats.map((plane) => (
-    <div key={plane.id} className="contain">
+    <div key={plane.id} className="flex contents">
       <div className="image">
         <img
-          className="d-block w-100 img"
+          className="d-block w-9 h-48 img inset-auto md:w-full md:h-60 rounded-2xl hover:shadow-2xl hover:shadow-gray-600 transition-all"
           src={plane.images}
           alt="airplane"
         />
       </div>
-      <div className="info">
-        <h2>{plane.name}</h2>
+      <div className="info h-9 w-56 fixed inset-1/2 bg-gray-900 bg-opacity-75 rounded-t-md">
+        <h2 className="cool-title my-auto text-center capitalize text-white h-full">{plane.name}</h2>
         <div>
           <p>capacity</p>
           <p>{plane.capacity}</p>
@@ -46,20 +46,22 @@ export default function Details() {
             {plane.price}
           </p>
         </div>
-        <NavLink to="/add-reservation" className="btn">
-          <p>
-            <FaRegSun />
-            {' '}
-            Reserve
-            <FaRegArrowAltCircleRight />
-          </p>
-        </NavLink>
-        <br />
-        <NavLink to="/" className="btn">
-          <p>
-            <FaRegArrowAltCircleLeft />
-          </p>
-        </NavLink>
+        <div className="flex hover:bg-none mb-9 object-center">
+          <NavLink to="/add-reservation" className="btn">
+            <p>
+              <FaRegSun />
+              {' '}
+              Reserve
+              <FaRegArrowAltCircleRight />
+            </p>
+          </NavLink>
+          <br />
+          <NavLink to="/" className="btn">
+            <p>
+              <FaRegArrowAltCircleLeft />
+            </p>
+          </NavLink>
+        </div>
       </div>
     </div>
   ));
