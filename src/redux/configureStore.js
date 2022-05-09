e1ec4e021c3baa95b6ed4a870a077af77735101f
Kingstalux <kingsleyngu25@gmail.com>
2022-05-09 10:10:43 +0100

    Fix details page

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,13 @@ import { applyMiddleware, combineReducers, createStore } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 import planeReducer from './airplanes/airplanes';
+import detailsReducer from './details/details';
 import reservationsReducer from './reservations/reservations';
 
 const rootReducer = combineReducers({
   airplanes: planeReducer,
   reservations: reservationsReducer,
+  details: detailsReducer,
 });
 
 const store = createStore(
