76995bfa58b6c8fc6b81c122e547c6c37297723c
Kingstalux <kingsleyngu25@gmail.com>
2022-05-10 14:22:15 +0100

    Fix linters

--- src/pages/Reserve.js
+++ src/pages/Reserve.js
@@ -1,29 +1,30 @@
 import { useState } from 'react';
 
-export default function Reservation (){
+export default function Reservation() {
   const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
   const handleSubmit = (event) => {
     event.preventDefault();
-    reserve(from,to);
+    // reserve(from, to);
   };
 
   return (
-      <form className='login-form' onSubmit={handleSubmit}>
-  <h2>Today's Date</h2>
-  <lable className='lable'>
-    <span>From:</span>
-    <input type="date" onChange={(e) => setFrom(e.target.value)} value={from} />
-  </lable>
+    <form className="login-form" onSubmit={handleSubmit}>
+      <h2>Reservation</h2>
+      <lable className="lable">
+        <span>From:</span>
+        <input type="date" onChange={(e) => setFrom(e.target.value)} value={from} />
+      </lable>
 
-  <lable className='lable'>
-    <span>To:</span>
-    <input type="date" onChange={(e) => setTo(e.target.value)} value={to} />
-  </lable>
+      <lable className="lable">
+        <span>To:</span>
+        <input type="date" onChange={(e) => setTo(e.target.value)} value={to} />
+      </lable>
 
-  <lable className='lable'>
-    <button className='btn'>Add Reservation</button>
-  </lable>
+      <lable className="lable">
+        <button className="btn" type="button">Add Reservation</button>
+      </lable>
 
-</form>)
+    </form>
+  );
 }
