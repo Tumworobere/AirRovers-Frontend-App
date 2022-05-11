a90f53b3c4917bc249075ace8673ce818e34d7de
Tchilo <tchiloross@gmail.com>
2022-05-11 09:30:59 +0200

    Add handle submit function

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -2,6 +2,7 @@
 /* eslint-disable no-unused-expressions */
 /* eslint-disable no-useless-escape */
 import React, { useEffect } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { register } from '../logic/api';
 import { topDown, downTop } from '../animations';
@@ -13,36 +14,42 @@ const Register = () => {
   useEffect(() => {
     topDown();
   }, []);
+
+  const [email, setEmail] = useState('');
+  const [name, setName] = useState('');
+
+  const [password, setPassword] = useState('');
+const { error, isPending, signup } = useSignup();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    signup(name, email, password)
+  }
+
   sessionStorage.getItem('token') !== null ? navigate('/') : null;
   return (
     <div className="h-screen w-screen">
       <div id="main" className="opacity-0 -translate-y-full block p-5 rounded-lg shadow-lg bg-white w-3/4 md:w-1/2 lg:w-1/3 h-auto m-auto mt-30 transition-all duration-1000">
         <h1 className="mt-3 text-center text-3xl font-bold text-gray-900">Register </h1>
-        <form id="form">
+        <form id="form" onSubmit={handleSubmit}>
           <div className=" mb-4">
             <label htmlFor="user" className="inline-block text-gray-700 w-full">
               Username
               <br />
-              <input type="text" id="name" placeholder="Enter username" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+              <input onChange={(e) => setName(e.target.value)}value={name} type="text" id="name" placeholder="Enter username" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
             </label>
           </div>
           <div className=" mb-4">
             <label htmlFor="email" className="inline-block text-gray-700 w-full">
               Email
               <br />
-              <input type="email" id="email" placeholder="Enter email address" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
-            </label>
-          </div>
-          <div className=" mb-4">
-            <label htmlFor="password" className="inline-block mb-2 text-gray-700 w-full">
-              Password
-              <input id="password" type="password" placeholder="Enter password" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+              <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" id="email" placeholder="Enter email address" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
             </label>
           </div>
           <div className=" mb-6">
-            <label htmlFor="confirmPassword" className="inline-block mb-2 text-gray-700 w-full">
-              Confirm Password
-              <input id="confirmPassword" type="password" placeholder="Confirm previous password" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
+            <label htmlFor="password" className="inline-block mb-2 text-gray-700 w-full">
+              Password
+              <input id="password" onChange={(e) => setPassword(e.target.value)} value={password} type="password" placeholder="Enter password" required className="mt-2 appearance-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" />
             </label>
           </div>
           <button
@@ -51,7 +58,7 @@ const Register = () => {
         px-6
         py-2.5
         bg-emerald-800
-        text-white
+        text-black
         font-medium
         text-m
         leading-tight
