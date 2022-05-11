d0e9de79857055502e9a7589e10329eb265d1072
alexander16108 <alexandermayowa05@gmail.com>
2022-05-11 17:15:09 +0100

    adding logic and styles for remove airplane

---
+++ src/pages/RemoveAirplane.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-expressions */
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { topDown } from '../animations';
+import planeCard from '../components/planeCard';
+
+const RemoveAirplane = () => {
+  const navigate = useNavigate();
+  const airplanes = useSelector((state) => state.airplanes.airplanes_arr);
+  useEffect(() => {
+    window.sessionStorage.getItem('token') == null ? navigate('/login') : null;
+    topDown();
+  }, []);
+  return (
+    <div id="main" className="p-4 md:p-24 flex justify-center flex-col w-full md:w-3/4 lg:w-5/6 transition-all opacity-0 -translate-y-full origin-left ease-out duration-500">
+      <h1 className="cool-title text-center !text-3xl md:!text-5xl">Delete an Airplane</h1>
+      <hr className="h-1 border-slate-700 mt-4" />
+      <ul className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 justify-evenly mt-20 m-auto w-full h-full" style={{ gap: '20px' }}>
+        {airplanes.map((airplane) => (
+          <planeCard key={airplane.id} airplane={airplane} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default RemoveAirplane;
