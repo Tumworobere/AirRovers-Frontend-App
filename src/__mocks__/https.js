d44d7a7218b0f72310eb74e9deb45c77aca5edd3
Tchilo <tchiloross@gmail.com>
2022-05-13 12:57:55 +0200

    Add test ans mocks file

---
+++ src/__mocks__/https.js
@@ -0,0 +1,32 @@
+const fetchData = () => Promise.resolve([
+  {
+    id: 1,
+    name: "Lighter-than-air",
+    capacity: 2,
+    range: "1,000 miles",
+    url: "https://www.jetblue.com/",
+    speed: "1,500 mph",
+    city: "New York",
+    images: "https://upload.wikimedia.org/wikipedia/commons/a/a2/Me_262_flight_show_at_ILA_2006_%28cropped%29.jpg",
+    price: "8687",
+    reservation_expiration_date: null,
+    created_at: "2022-05-06T04:25:43.904Z",
+    updated_at: "2022-05-06T04:25:43.904Z"
+    },
+    {
+    id: 2,
+    name: "AH-64 Apache helicopter",
+    capacity: 2,
+    range: "1,000 miles",
+    url: "https://www.jetblue.com/",
+    speed: "1,500 mph",
+    city: "New York",
+    images: "https://cdn.britannica.com/20/27120-050-53824E0C/attack-AH-64-Apache-reconnaissance-helicopter-prototype-form-1975.jpg",
+    price: "26429",
+    reservation_expiration_date: null,
+    created_at: "2022-05-06T04:25:44.010Z",
+    updated_at: "2022-05-06T04:25:44.010Z"
+    },
+]);
+
+export default fetchData;
